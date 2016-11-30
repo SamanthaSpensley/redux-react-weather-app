8071b6d6ed66c17bae01688d3fa1173779d49945
Samantha Spensley <spenslsm@gmail.com>
2016-11-30 08:55:03 -0700

    weather reducer and table

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = 'a27bc89fa277c33ef6026b567c7441b0';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url) // use axios for AJX request
+
+  // console.log('Request:', request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,11 @@
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+  case FETCH_WEATHER:
+    // DONT mutate state (aka state.push(action.payload.data)), concat doesn't mutate, rather returns a new array
+    // return state.concat([action.payload.data]); --> one option
+    return [ action.payload.data, ...state ]; // ES6 array
+  }
+  return state;
+}
